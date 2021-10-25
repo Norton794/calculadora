71d6fe65a44230f159933f6b3db2de3bcab3b864
Norton dos Santos <nortonsantos79@gmail.com>
2021-10-25 17:38:55 -0300

    componente Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+  const style = {
+    fontSize: "1.4em",
+    backgroundColor: "#fff",
+    border: "none",
+    borderRight: "solid 1px #888",
+    borderBottom: '1px solid #888',
+  };
+
+  return <button style={style} className="button">0</button>;
+};
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Calculator.css";
-
-export default class Calculator extends React.Component{
-    render(){
-        return(
-            <div className="calculator">
-                
-            </div>
-        );
-    }
+import Button from "../components/Button";
+export default class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calculator">
+        <Button />
+      </div>
+    );
+  }
 }

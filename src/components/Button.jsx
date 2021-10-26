08d9c8640647af554b89633cfe05f0506a610cae
Norton dos Santos <nortonsantos79@gmail.com>
2021-10-26 12:24:29 -0300

    terminei os estilos e adicionei alguns metodos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-
-export default ({label}) => {
+import "./Button.css";
+export default ({ label, operation, double, triple, click }) => {
   const style = {
     fontSize: "1.4em",
     backgroundColor: "#fff",
     border: "none",
     borderRight: "solid 1px #888",
-    borderBottom: '1px solid #888',
-    outline: 'none',
+    borderBottom: "1px solid #888",
+    outline: "none",
   };
 
-  return <button style={style} className="button">{label}</button>;
+  return (
+    <button
+    onClick={e => click && click(label)}
+      style={style}
+      className={
+        `button 
+            ${operation ? "operation" : ""} 
+            ${double ? "double" : ""} 
+            ${triple ? "triple" : ""} 
+      `}
+    >
+      {label}
+    </button>
+  );
 };
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,3 +1,3 @@
 import React from 'react';
-
+import './Display.css';
 export default ({value}) => <div className="display">{value}</div>
